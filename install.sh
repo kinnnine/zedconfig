8a5b329661e6e5fc1b7c63bc84f9163a3abccb5c
kinnnine <nakaharaml9@gmail.com>
2026-04-14 02:49:42 +0700

    install.sh: Don't copy hidden files/directories.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 Z="$HOME/.config/zed"
 [ -d "$Z" ] && echo "Remove old config first, aborting." && exit 1
-mkdir "$Z" && cp -rv . "$Z" && rm -v "$Z"/*.sh "$Z"/*.md
+mkdir "$Z" && cp -rv * "$Z" && rm -v "$Z"/*.sh "$Z"/*.md
